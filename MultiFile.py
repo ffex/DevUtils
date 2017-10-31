44732b824444ef8839c5ed563bd5ec485e4cad88
ffex <kurogan21@gmail.com>
2017-10-31 16:04:32 +0100

    Create MultiFile.py

---
+++ MultiFile.py
@@ -0,0 +1,12 @@
+import os
+
+class MultiFile:
+    def __init__(self,type):
+        self._type=type
+        self._dir=
+        self._dict={}
+        
+    def loadTitles(self):
+        
+        
+    
