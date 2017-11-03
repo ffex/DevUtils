b2636bb75ef0082a76fd617bef1672a05c46253d
ffex <kurogan21@gmail.com>
2017-11-03 09:47:00 +0100

    Update MultiFile.py

--- MultiFile.py
+++ MultiFile.py
@@ -4,12 +4,26 @@ class MultiFile:
     def __init__(self,type):
         self._type=type
         path=os.path.join(os.path.realpath(__file__),type)
-        if (os.path.isdir(path)):
-            
+        if !(os.path.isdir(path)):
+            os.makedirs(path)
         self._dir=os.path.dirname(os.path.realpath(__file__))
-        self._dict={}
+        self._titles=[]
+        self.loadTitles()
+        
         
     def loadTitles(self):
-        
-        
+        if os.path.isdir(self._dir):
+            dirs = os.listdir(path)
+            for file in dirs:
+                self._titles.append(file)
+                
     
+    def getTitles(self):
+        if len(self._titles)>0:
+            return self._titles
+        else
+            return 0
+    def addFile(self):
+        
+    def deleteFile(self):
+        
