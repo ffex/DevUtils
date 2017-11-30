15f32bffc2e2166abd558b51a470f0010f428827
ffex <kurogan21@gmail.com>
2017-11-30 08:46:45 +0100

    add controls for commands

--- Core/Commands.py
+++ Core/Commands.py
@@ -3,4 +3,30 @@ import os
 #format:
 #"n,a,d,q,v"
 def isCommandOK(cformat,command):
-    pass
+    result=False
+	acceptedCom=cformat.Split(',')
+	if (len(command)==1 or (len(command)>=2 and command[1]==',')):
+	    for x in acceptedCom:
+		    if command[0]==x:
+			    result=True
+	else:
+	    result=False
+	return result
+
+def isLink(URL):
+    result=False
+	if URL.startswith('http://'):
+	    result=True
+	if URL.startswith('https://'):
+	    result=True
+	if URL.startswith('www'):
+	    result==True
+	return result
+	
+def isDirectory(dirname):
+    result=False
+	if dirname.startswith('/'):
+	    result=True
+	if dirname.startswith('$'):
+	    result=True
+	return result
